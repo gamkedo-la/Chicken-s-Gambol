c3fce5d1a90c1dc18efa68a9f8cab88982c6e9b8
stebssbets <32585154+stebssbets@users.noreply.github.com>
2018-11-16 19:28:32 -0500

    chicken idle sounds added, source recordings in source assets folder, audio.js created, separated the sounds into 3 arrays (low pitch, medium pitch, high pitch) for an attempt of creating a 3 different chicken effect, randomized the assignments for aural variety, also added reassignment to the onended property of the audio tag for further aural variety

---
+++ js/audio.js
@@ -0,0 +1,36 @@
+let idleChickenSound1 = new Audio();
+let idleChickenSound2 = new Audio();
+let idleChickenSound3 = new Audio();
+
+let arrayOfIdleChicken1Sounds = new Array();
+let arrayOfIdleChicken2Sounds = new Array();
+let arrayOfIdleChicken3Sounds = new Array();
+
+arrayOfIdleChickenSounds1 = ["audio/chicken_idle/chicken_cup_1.mp3", "audio/chicken_idle/chicken_cup_2.mp3", "audio/chicken_idle/chicken_cup_3.mp3"];
+arrayOfIdleChickenSounds2 = ["audio/chicken_idle/chicken_cup_4.mp3", "audio/chicken_idle/chicken_cup_5.mp3", "audio/chicken_idle/chicken_cup_7.mp3"];
+arrayOfIdleChickenSounds3 = ["audio/chicken_idle/chicken_cup_8.mp3", "audio/chicken_idle/chicken_cup_9.mp3"];
+
+function randomInt(min, max) {
+  return Math.floor(min + Math.random() * (max + 1 - min))
+}
+
+function assignIdleChickenSound(arrayOfIdleChickenSounds) {
+  let length = arrayOfIdleChickenSounds.length;
+  let randomIdleChickenSoundsIndex = randomInt(0, length - 1);
+  return arrayOfIdleChickenSounds[randomIdleChickenSoundsIndex];
+}
+
+idleChickenSound1.src = assignIdleChickenSound(arrayOfIdleChickenSounds1);
+idleChickenSound1.autoplay = true;
+idleChickenSound1.loop = true;
+idleChickenSound1.onended = assignIdleChickenSound(arrayOfIdleChickenSounds1);
+
+idleChickenSound2.src = assignIdleChickenSound(arrayOfIdleChickenSounds2);
+idleChickenSound2.autoplay = true;
+idleChickenSound2.loop = true;
+idleChickenSound2.onended = assignIdleChickenSound(arrayOfIdleChickenSounds2);
+
+idleChickenSound3.src = assignIdleChickenSound(arrayOfIdleChickenSounds3);
+idleChickenSound3.autoplay = true;
+idleChickenSound3.loop = true;
+idleChickenSound3.onended = assignIdleChickenSound(arrayOfIdleChickenSounds3);
