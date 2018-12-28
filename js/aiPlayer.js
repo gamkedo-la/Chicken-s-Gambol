4f7d5a573921cd9585cf8f33e8865e21b6c1be1e
Brian Boucher <junkyardconcierge@gmail.com>
2018-12-28 10:32:37 -0600

    AI player can find all of its own units.

--- js/aiPlayer.js
+++ js/aiPlayer.js
@@ -1,13 +1,25 @@
 const AIPlayer = new (function() {
 	
-	let elapsedSinceUpdate = 0;
-	const updateInterval = 0.05;
+  let elapsedSinceUpdate = 0;
+  const updateInterval = 1;
 	
-	this.update = function (delta){
-		elapsedSinceUpdate += delta;
-		if (updateInterval <= elapsedSinceUpdate) {
-			
+  this.update = function (delta){
+    elapsedSinceUpdate += delta;
+    if (updateInterval <= elapsedSinceUpdate) {
+	  elapsedSinceUpdate = 0;
+	  this.findAllEnemyUnits();
+    }
+  };
+	
+  //To test AI functionality
+  this.findAllEnemyUnits = (function(){
+    let length = Game.units.length;
+      for (let i = 0; i < length; i++) {
+        let unit = Game.units[i];
+        if (unit.isPlayer(TEAM_ENEMY)) {
+		  //console.log("Enemy unit at " + unit.x + "," + unit.y + " found");
 		}
-	};
+	  }
+  });
 
 })();	
